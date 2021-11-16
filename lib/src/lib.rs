bd49dbb74a671206f3f8538ade9358fe87accac6
unic0rn9k <unic0rn9k@protonmail.com>
2021-11-16 16:00:04 +0100

    Implemented moves for Pawn

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -13,6 +13,14 @@ pub enum Piece {
     None,
 }
 
+#[wasm_bindgen]
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Color {
+    WHITE,
+    BLACK,
+}
+use Color::*;
+
 // (1, A) / (0, 0) er nede til venstre. (8, H) / (7, 7) er oppe til højre.
 const BOARD_SIZE: u8 = 8;
 
@@ -24,12 +32,24 @@ fn pos_to_u16(x: u8, y: u8) -> u16 {
 }
 
 #[wasm_bindgen]
-pub fn valid_moves(piece: Piece, pos: u16, color: bool) -> Vec<u16> {
-    let mut not_gay_array = pos.to_ne_bytes();
+pub fn valid_moves(piece: Piece, pos: u16, color: Color) -> Vec<u16> {
+    let pos = pos.to_ne_bytes();
     let mut buffer = vec![];
 
     match piece {
-        Piece::Pawn => todo!(),
+        Piece::Pawn => {
+            if color == WHITE {
+                buffer.push(pos_to_u16(pos[0], pos[1] + 1));
+                if pos[1] == 2 {
+                    buffer.push(pos_to_u16(pos[0], pos[1] + 2));
+                }
+            } else {
+                buffer.push(pos_to_u16(pos[0], pos[1] - 1));
+                if pos[1] == 6 {
+                    buffer.push(pos_to_u16(pos[0], pos[1] - 2));
+                }
+            }
+        }
         Piece::Queen => todo!(),
         Piece::King => todo!(),
         Piece::Knight => todo!(),
