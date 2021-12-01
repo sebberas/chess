be7497695ab01fb660a8b5e5b24f548c9cdd4ca2
unic0rn9k <unic0rn9k@protonmail.com>
2021-12-01 14:15:53 +0100

    Stuff

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -23,12 +23,13 @@ impl Board {
             let len = ((dx.powi(2) + dy.powi(2)) as f32).sqrt();
             dx = if len == 0. { 0. } else { dx / len };
             dy = if len == 0. { 0. } else { dy / len };
-            (dx.floor(), dy.floor(), len.floor())
+            (dx.round(), dy.round(), len)
         };
 
         for mv in &buffer {
             let (dx, dy, len) = vector_comp(&pos, mv);
             if self.0[mv.x as usize][mv.y as usize].0 != Piece::None {
+                println!("{:?} : {:?}", (mv.x, mv.y), (dx, dy, len));
                 dead_vecs.push((dx, dy, len));
             }
         }
@@ -41,9 +42,7 @@ impl Board {
                 }
                 let (dx, dy, len) = vector_comp(&pos, mv);
                 // !dead_arrows.iter().any(|a| a(mv.x) == mv.y)
-                !dead_vecs
-                    .iter()
-                    .any(|(x, y, slen)| *x == dx && *y == dy && len > *slen)
+                !dead_vecs.iter().any(|(x, y, slen)| *x == dx && *y == dy)
             })
             .copied()
             .collect()
@@ -52,18 +51,12 @@ impl Board {
 
 #[wasm_bindgen]
 extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
 
-    // The `console.log` is quite polymorphic, so we can bind it with multiple
-    // signatures. Note that we need to use `js_name` to ensure we always call
-    // `log` in JS.
     #[wasm_bindgen(js_namespace = console, js_name = log)]
     fn log_u32(a: u32);
 
-    // Multiple arguments too!
     #[wasm_bindgen(js_namespace = console, js_name = log)]
     fn log_many(a: &str, b: &str);
 }
@@ -71,7 +64,7 @@ extern "C" {
 // Main function for debugging
 #[wasm_bindgen]
 pub fn main() {
-    let mut board = ['#'; 8 * 8];
+    let mut board = [' '; 8 * 8];
 
     let mut game = Board([[(Piece::None, White); 8]; 8]);
     game.0[5][5].0 = Piece::Pawn;
