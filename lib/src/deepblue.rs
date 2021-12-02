0da756dff5b4c23781aecee2bd816655353ac2f8
unic0rn9k <unic0rn9k@protonmail.com>
2021-12-02 14:02:13 +0100

    [WIP] naive_value function for deepblue algorithm

--- lib/src/deepblue.rs
+++ lib/src/deepblue.rs
@@ -26,3 +26,51 @@ impl GameState {
         }
     }
 }
+
+#[derive(PartialEq, PartialOrd, Clone, Copy)]
+enum Value {
+    NegInf,
+    Num(i32),
+    Inf,
+}
+
+use Value::*;
+
+fn piece_value(p: Piece) -> i32 {
+    use Piece::*;
+    match p {
+        Pawn => 1,
+        Knight => 3,
+        Bishop => 3,
+        Rook => 5,
+        Queen => 9,
+        King => 100,
+        None => 0,
+    }
+}
+
+impl Board {
+    fn naive_value(&self, color: Color) -> Value {
+        let mut val = 0;
+        let mut sk = false; // Does 'color' have a king?
+        let mut ok = false; // Does other color have a king?
+
+        for p in self.0.iter().flatten() {
+            if p.1 == color {
+                sk = p.0 == Piece::King || sk;
+                val += piece_value(p.0);
+            } else {
+                ok = p.0 == Piece::King || ok;
+                val -= piece_value(p.0);
+            }
+        }
+
+        if !sk {
+            NegInf
+        } else if !ok {
+            Inf
+        } else {
+            Num(val)
+        }
+    }
+}
